910cfcd83cf07916322a33da12baea27bb211a27
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2021-11-15 14:57:07 +0800

    fix: 删除过期文件 (#15)
    Co-authored-by: jeessy2 <jeessy2@users.noreply.github.com>

--- util/docker_util.go
+++ util/docker_util.go
@@ -2,14 +2,11 @@ package util
 
 import "os"
 
-// DockerEnvFile Docker容器中包含的文件
-const DockerEnvFile string = "/.dockerenv"
+// dockerEnvFile Docker容器中包含的文件
+const dockerEnvFile string = "/.dockerenv"
 
 // IsRunInDocker 是否在docker中运行
 func IsRunInDocker() bool {
-	_, err := os.Stat(DockerEnvFile)
-	if err != nil {
-		return false
-	}
-	return true
+	_, err := os.Stat(dockerEnvFile)
+	return err == nil
 }
